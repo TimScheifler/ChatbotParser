b0b1f095de720d06bd65f12dbbed99c1258bf8c1
Tim Scheifler <32902115+TimScheifler@users.noreply.github.com>
2022-07-27 13:52:06 +0200

    - added rasa endpoints

--- app/botpress.py
+++ app/botpress.py
@@ -1,4 +1,5 @@
-from .botpress_secrets import secrets
+from .botpress_secrets import secrets as botpress_secrets
+from .rasa_secrets import secrets as rasa_secrets
 from time import time
 from fastapi import FastAPI, Header, Request
 from typing import Union
@@ -45,20 +46,36 @@ async def getBotpressInterventionResponse(request: Request):
 
 @app.get("/botpress/jwt")
 async def getBotpressJwt():
-    host = secrets.get('IP')
-    port = secrets.get('PORT')
+    host = botpress_secrets.get('IP')
+    port = botpress_secrets.get('PORT')
+    pw = botpress_secrets.get('PW')
+    email = botpress_secrets.get('EMAIL')
 
     auth_url = 'http://' + host + ':' + port + '/api/v1/auth/login/basic/default';
-    auth_payload = {'email': secrets.get('EMAIL'), 'password': secrets.get('PW')}
+    auth_payload = {'email': email, 'password': pw}
 
     response = requests.post(auth_url, json=auth_payload)
 
     return { "jwt":response.json()['payload']['jwt']}
 
 
-#@app.get("/sessionuuid")
-#async def getSessionUuId(participant_uuid: str):
-#    return {"session_uuid":participant_uuid + "_" + str(time())}
+@app.get("/rasa/intervention")
+async def getRasaInterventionResponse(request: Request):
+    body = await request.json()
+
+    msg = body['msg']
+    session_uuid = body['session_uuid']
+
+    return __sendMessageToRasa(msg, session_uuid)
+
+@app.get("/rasa/faq")
+async def getRasaFaqResoinse(request: Request):
+    body = await request.json()
+
+    msg = body['msg']
+    session_uuid = body['session_uuid']
+
+    return __sendMessageToRasa(msg, session_uuid + '_faq')
 
 @app.get("/")
 async def greet():
@@ -66,9 +83,9 @@ async def greet():
 
 
 def __sendMessageToBotpress(msg, session_uuid, jwt):
-    host = secrets.get('IP')
-    port = secrets.get('PORT')
-    bot_name = secrets.get('BOT')
+    host = botpress_secrets.get('IP')
+    port = botpress_secrets.get('PORT')
+    bot_name = botpress_secrets.get('BOT')
 
     LOGGER.warning("TEEEST: " + msg + " " + session_uuid + " " + jwt)
 
@@ -97,6 +114,46 @@ def __sendMessageToBotpress(msg, session_uuid, jwt):
             else:
                 answer['data']['flow'] = text
                 text = ""
+        else:
+            answer['data']['done'] = 'false'
+            if counter > 0:
+                text = text + '---'
+            text = text + element['text']
+        counter = counter + 1
+
+    answer['data']['message'] = text
+    return answer
+
+def __sendMessageToRasa(msg, session_uuid):
+    host = rasa_secrets.get('IP')
+    port = rasa_secrets.get('PORT')
+    token = rasa_secrets.get('TOKEN')
+
+    url = "http://" + host + ":" + port + "/webhooks/rest/webhook?token=" + token
+
+    payload = {"sender": session_uuid, "message": msg}
+    # headers = {'content-type': 'application/json'}
+
+    response = requests.post(url, json=payload)
+
+    data = response.json()
+
+    answer = {}
+    text = ''
+    counter = 0
+    answer['type'] = 'message'
+    answer['data'] = {}
+    answer['data']['done'] = 'true'
+    answer['data']['flow'] = 'default'
+
+    for element in data:
+        answer['data']['answer_type'] = 'text'
+        if element['text'] == '$end':
+            answer['data']['done'] = 'true'
+            if '---' in text:
+                answer['data']['flow'] = text.split('---')[-1]
+            else:
+                answer['data']['flow'] = text
         else:
             answer['data']['done'] = 'false'
             if counter > 0:
